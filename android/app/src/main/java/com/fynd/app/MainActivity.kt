c7da3be9c70ee72fba60239171a5203f53d79c7e
jallohosmanamadu <jallohosmanamadu311@gmail.com>
2026-03-04 19:21:53 -0500

    fix: rename Android package from com.anonymous.fyndapp to com.fynd.app

--- android/app/src/main/java/com/fynd/app/MainActivity.kt
+++ android/app/src/main/java/com/fynd/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.fyndapp
+package com.fynd.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/fynd/app/MainApplication.kt
+++ android/app/src/main/java/com/fynd/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.fyndapp
+package com.fynd.app
 
 import android.app.Application
 import android.content.res.Configuration
